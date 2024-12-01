eab674a7992da7cabc271510121948bce42df3e7
Арсений <gl.arseniy@mail.ru>
2024-12-01 22:21:08 +0300

    Added table rates

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -4,6 +4,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from contextlib import asynccontextmanager
 
 from .base import Base
+from .rate import Rate
 
 class DataBaseHelper:
     def __init__(
--- app/models/rate.py
+++ app/models/rate.py
@@ -13,15 +13,7 @@ from .base import Base
 
 class Rate(Base):
     __tablename__ = "rates"
-    type_id: Mapped[int] = mapped_column(Integer, ForeignKey('cargo_types.id'), 
-            nullable=False)
+    title: Mapped[str] = mapped_column(String(128), nullable=False)
     value: Mapped[int] = mapped_column(Integer, nullable=False)
     time_created: Mapped[datetime] = mapped_column(DateTime, nullable=False,
             server_default=func.now())
-    
-
-class CargoType(Base):
-    __tablename__ = "cargo_types"
-    title: Mapped[str] = mapped_column(String(128), nullable=False)
-    time_created: Mapped[datetime] = mapped_column(DateTime, nullable=False,
-        server_default=func.now())
